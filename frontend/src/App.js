f0bc4a2addaf5ad12f46b5a21ed74ab7b210653f
Tyler Kierstein <35084718+VulshokBersrker@users.noreply.github.com>
2023-10-16 19:24:23 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Products from './pages/about';
+import Register from './pages/register';
+import About from './pages/about';
+import SignIn from './pages/signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/products' component={Products} />
+        <Route path='/about' component={About} />
+        <Route path='/register' component={Register} />
+        <Route path='/signin' component={SignIn} />
+      </Routes>
+    </Router>
   );
 }
 
