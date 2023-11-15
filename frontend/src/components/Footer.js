cb49c295a48fb1bc71b3617a7adc60db5348c019
Tyler Kierstein <35084718+VulshokBersrker@users.noreply.github.com>
2023-11-14 20:48:09 -0500

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,9 +15,9 @@ import EmailIcon from '@mui/icons-material/Email';
 const Footer = () => {
     return (
         <div>
-            <div className='pb-5 px-5'>
-                <div className="row">
-                    <div className="col-2 px-4 pt-5">
+            <div className='px-5'>
+                <div className="row footerWrapper">
+                    <div className="col-2 px-4 pt-1">
                         <div className = "d-flex justify-content-center py-3">
                             <div className = "footerLogo footerLogoWrapper"></div>
                         </div>
@@ -28,7 +28,7 @@ const Footer = () => {
                         </div>
                     </div>
                   
-                    <div className="col col-xl pt-5">
+                    <div className="col col-lg pt-5">
                         <div className='row'>
                             <div className="col border">
                                 <div className="footerSubTitle pb-2">shop</div>
@@ -49,25 +49,18 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="col-3">
-                        <div className = "row py-4">
-                            <div className = "d-flex footerTitle text-black pb-2">about us</div>
+                        <div className="d-flex row pt-3">
+                            <div className="d-flex footerTitle text-black pb-1">contact us</div>
                             <div class="pt-1 bg-danger bg-gradient w-100" />
-                            <div className = "pt-3 d-flex justify-content-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Nunc sodales, risus mollis blandit consequat, libero sem 
-                            molestie dolor, non convallis diam felis ut velit. Integer interdum lorem ex, quis congue dolor tincidunt id.</div>
-                        </div>
-                        <div className = "d-flex row py-3">
-                            <div className="d-flex footerTitle text-black pb-2">contact us</div>
-                            <div class="pt-1 bg-danger bg-gradient w-100" />
-                            <div className="py-1 pt-3"><PhoneIcon className='socialIcons' />       123-456-7890</div>
+                            <div className="py-1 pt-4"><PhoneIcon className='socialIcons' />       123-456-7890</div>
                             <div className="py-1"><EmailIcon className='socialIcons' />       info@customframe.com</div>
                             <div className="py-1"><LocationOnIcon className='socialIcons' />      123 West Orlando Dr, Oviedo, FL, 12345</div>
                         </div>
                     </div>
                 </div>
-          </div>
-          <div className="d-flex justify-content-center bg-dark py-1 text-white"> ©2023 MyCustomFrame. All Rights Reserved E-Commerce Web Design by SDT8</div>
-      </div>
+            </div>
+            <div className="d-flex justify-content-center bg-dark py-1 text-white"> ©2023 MyCustomFrame. All Rights Reserved E-Commerce Web Design by SDT8</div>
+        </div>
     );
 };
 
