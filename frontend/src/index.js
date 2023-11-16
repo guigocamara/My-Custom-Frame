b3f8acca5ecb46b4285b4db985458b2c89e0b2d2
Tyler Kierstein <35084718+VulshokBersrker@users.noreply.github.com>
2023-11-16 12:49:45 -0500

    Updated Files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,26 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebase from "firebase";
+//import firebase from "firebase";
 
-firebase.initializeApp({
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
   apiKey: "AIzaSyBvuImOmZc-imfhQAs7CuwF8QQPXyBz_3o",
-
   authDomain: "mycustomframe-93aef.firebaseapp.com",
-
   databaseURL: "https://mycustomframe-93aef-default-rtdb.firebaseio.com",
-
   projectId: "mycustomframe-93aef",
-
   storageBucket: "mycustomframe-93aef.appspot.com",
-
   messagingSenderId: "318772604991",
-
   appId: "1:318772604991:web:983887767f4ba760dac88a",
-
   measurementId: "G-LZZYMDWV2V"
-});
+}
 
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
